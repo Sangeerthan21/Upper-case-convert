4bf19dfa682c3527952c85bc57da7f49318b4910
Sangeerthan21 <kandeepann5@gmail.com>
2024-12-06 18:14:09 +0530

    String inbuilt

---
+++ activity.py
@@ -0,0 +1,4 @@
+lower = input("Enter the word you want to convert : ")
+print( "The word is seen as", lower.upper(), "in upper case")
+print( "The word is seen as", lower.lower(), "in lower case")
+print( "The word is seen as", ''.join(reversed(lower)), "in reverse order")
